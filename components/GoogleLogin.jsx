6f899a0d6a56775227abe6eedac911217e3b8ff7
MIMD-DanielChagas <168031567+MIMD-DanielChagas@users.noreply.github.com>
2026-02-09 22:17:46 -0300

    Update GoogleLogin.jsx

--- components/GoogleLogin.jsx
+++ components/GoogleLogin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 
 const GoogleLoginComponent = ({ onLoginSuccess }) => {
@@ -33,17 +33,15 @@ const GoogleLoginComponent = ({ onLoginSuccess }) => {
   };
 
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <div style={{ textAlign: 'center', padding: '20px' }}>
-        <h2>Conectar com Google</h2>
-        <GoogleLogin
-          onSuccess={handleSuccess}
-          onError={handleError}
-          text="signin_with"
-          size="large"
-        />
-      </div>
-    </GoogleOAuthProvider>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h2>Conectar com Google</h2>
+      <GoogleLogin
+        onSuccess={handleSuccess}
+        onError={handleError}
+        text="signin_with"
+        size="large"
+      />
+    </div>
   );
 };
 
